ea41b187dad985e97671d0bbac81bbe529480312
RaquelMalinis <39636332+RaquelMalinis@users.noreply.github.com>
2019-10-03 15:54:02 -0600

    Added a few more comments
    Changed parameter name for HAL_Delay from DLY to milliSeconds

--- basic_eve_demo_source_code/basic_eve_demo/include/hw_api.h
+++ basic_eve_demo_source_code/basic_eve_demo/include/hw_api.h
@@ -13,7 +13,7 @@ uint8_t HAL_SPI_Write(uint8_t data);
 uint8_t HAL_SPI_WriteByte(uint8_t data);
 void HAL_SPI_WriteBuffer(uint8_t *Buffer, uint32_t Length);
 void HAL_SPI_ReadBuffer(uint8_t *Buffer, uint32_t Length);
-void HAL_Delay(uint32_t DLY);
+void HAL_Delay(uint32_t milliSeconds);
 void HAL_Eve_Reset_HW(void);
 void HAL_Close(void);
 
