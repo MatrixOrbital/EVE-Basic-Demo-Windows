1fb6f222af1bf57fba25292deee9071dee8d7c06
Raquel Malinis <rmalinis@matrixorbital.ca>
2021-02-19 14:37:39 -0700

    Added EVE4x displays: 10.1" and 4.0" display definitions and timing

--- basic_eve_demo_source_code/basic_eve_demo/include/MatrixEve2Conf.h
+++ basic_eve_demo_source_code/basic_eve_demo/include/MatrixEve2Conf.h
@@ -1,15 +1,17 @@
 //User selectable configurations.
 //#define EVE_70
 //#define EVE_50
-#define EVE_43
+//#define EVE_43
 //#define EVE_39
 //#define EVE_38
 //#define EVE_35
 //#define EVE_29
+//#define EVE_40
+#define EVE_101
 
 //Configure for the EVE board version you are using.
-#define EVE2
-//#define EVE3
+//#define EVE2
+#define EVE3 // EVE3, EVE3x or EVE4x boards
  
 //Configure what touch screen is being used. I no touch screen is used
 //comment out the following two lines
@@ -94,7 +96,7 @@
 #define VCYCLE     288
 #define VOFFSET     12
 #define VSYNC1       8
-#define VSYNC0      11
+#define VSYNC0       7
 #define PCLK         5
 #define SWIZZLE      0
 #define PCLK_POL     1
@@ -169,3 +171,48 @@
 #define CSPREAD      1
 #define DITHER       1
 #endif
+
+#ifdef EVE_40
+#define DWIDTH     720
+#define DHEIGHT    720
+#define PIXVOFFSET   0
+#define PIXHOFFSET   0
+#define HCYCLE     812
+#define HOFFSET     91
+#define HSYNC0      46
+#define HSYNC1      48
+#define VCYCLE     756
+#define VOFFSET     35
+#define VSYNC0      16
+#define VSYNC1      18
+#define PCLK         2
+#define SWIZZLE      0
+#define PCLK_POL     1
+#define HSIZE      720
+#define VSIZE      720
+#define CSPREAD      0
+#define DITHER       0
+#endif
+
+#ifdef EVE_101
+#define DWIDTH    1280
+#define DHEIGHT    800
+#define PIXVOFFSET   0
+#define PIXHOFFSET   0
+#define HCYCLE    1440
+#define HOFFSET    158
+#define HSYNC0      78
+#define HSYNC1      80
+#define VCYCLE     823
+#define VOFFSET     22
+#define VSYNC0      11
+#define VSYNC1      12
+#define PCLK         1
+#define SWIZZLE      0
+#define PCLK_POL     0
+#define HSIZE     1280
+#define VSIZE      800
+#define CSPREAD      0
+#define DITHER       1
+#endif
+
